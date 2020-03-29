4ed9bfc501542d2013c0ab2551e3ce194a1e2d01
lux <zhouluxiao@ouchteam.com>
2020-03-29 19:11:14 +0800

    update

--- app/src/main/java/com/topList/android/ui/subscribe/SubscribeHelper.kt
+++ app/src/main/java/com/topList/android/ui/subscribe/SubscribeHelper.kt
@@ -3,21 +3,22 @@ package com.topList.android.ui.subscribe
 import com.topList.android.api.NetResult
 import com.topList.android.api.model.State
 import com.topList.android.api.model.SubscribeItem
-import com.topList.android.ui.subscribe.model.CategoryItem
-import com.topList.android.ui.subscribe.model.StationUseItem
-import com.topList.android.ui.subscribe.model.SubscribeUseItem
+import com.topList.android.ui.subscribe.model.*
 import timber.log.Timber
 
 object SubscribeHelper {
     fun getCategoryData(result: NetResult<State<List<SubscribeItem>>>): SubscribeUseItem {
         val categoryList = mutableListOf<CategoryItem>()
-        val stationList = mutableListOf<StationUseItem>()
+        val stationList = mutableListOf<Station>()
+        val sourceList = mutableListOf<SubscribeItem>()
         when (result) {
             is NetResult.Success -> {
+                sourceList.addAll(result.data.data)
                 for (category in result.data.data) {
                     categoryList.add(CategoryItem(category.name, false))
+                    stationList.add(StationHeaderUseItem(category.name))
                     for (station in category.list) {
-                        stationList.add(StationUseItem(station))
+                        stationList.add(StationUseItem(station, category.name))
                     }
                 }
                 if (categoryList.size > 0)
@@ -27,6 +28,6 @@ object SubscribeHelper {
 
             }
         }
-        return SubscribeUseItem(categoryList, stationList)
+        return SubscribeUseItem(categoryList, stationList, sourceList)
     }
 }
--- app/src/main/java/com/topList/android/ui/subscribe/model/StationUseItem.kt
+++ app/src/main/java/com/topList/android/ui/subscribe/model/StationUseItem.kt
@@ -2,21 +2,32 @@ package com.topList.android.ui.subscribe.model
 
 import com.topList.android.api.model.StationItem
 
+open class Station(
+    var isHeader: Boolean
+)
+
 data class StationUseItem(
     val type: String,
     val name: String,
     val img: String,
     val id: String,
     val icon: String
-) {
-    var tag: String = ""
+) : Station(false) {
 
-    constructor(station: StationItem) :
+    var tag: String = name
+
+    constructor(station: StationItem, tag: String) :
             this(station.type,
                 station.name,
                 station.img,
                 station.id,
                 station.icon) {
-        this.tag = station.name
+        this.tag = tag
     }
+}
+
+data class StationHeaderUseItem(
+    val name: String
+) : Station(true) {
+    val tag = name
 }
